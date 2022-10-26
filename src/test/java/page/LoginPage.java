838a9245974729c3ca529249d40679847e396cbb
Andrea Garcia Rodriguez <agarciar@qvision.us>
2022-10-26 18:25:44 -0500

    carga arquetipo y selector

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -5,6 +5,4 @@ public class LoginPage {
     private static final String username_locator = "//input[@id='userName']";
     private static final String password_locator = "//input[@id='password']";
     private static final String buttonLogin_locator = "//button[@id='login']";
-
-
 }
